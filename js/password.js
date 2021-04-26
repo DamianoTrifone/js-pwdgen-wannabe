e7494804f1d8b881633ee43741e4514b15522fbb
DamianoTrifone <trifone.damiano@gmail.com>
2021-04-26 16:27:59 +0200

    Fine Javascript

--- js/password.js
+++ js/password.js
@@ -1,5 +1,10 @@
 var nome = prompt("Qual è il tuo nome?");
 console.log(nome);
 
-var cognome = promt("Qual è il tuo cognome?");
+var cognome = prompt("Qual è il tuo cognome?");
 console.log(cognome);
+
+var colore = prompt("Qual è il tuo colore preferito?");
+console.log(colore);
+
+document.write("La tua password (sconsigliata) è " + nome + cognome + colore + "21");
